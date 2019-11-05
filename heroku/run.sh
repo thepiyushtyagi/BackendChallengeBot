bc4e39715315f265c48f41e304f8cd89dbb619f3
Piyush Tyagi <piyush.tyagi@go-mac-piyusht.local>
2019-11-06 01:43:02 +0530

    /.

--- heroku/run.sh
+++ heroku/run.sh
@@ -1,6 +1,7 @@
-apt-get update &&
-apt-get install build-essential &&
-apt-get install tcl8.5 &&
+apt-get install sudo &&
+sudo apt-get update &&
+sudo apt-get install build-essential &&
+sudo apt-get install tcl8.5 &&
 wget http://download.redis.io/redis-stable.tar.gz &&
 tar xvzf redis-stable.tar.gz &&
 cd redis-stable &&
