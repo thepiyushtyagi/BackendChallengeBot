0959970fc9a6437f68a3a5d04948806938025cac
Piyush Tyagi <piyush.tyagi@go-mac-piyusht.local>
2019-11-05 23:50:53 +0530

    small change

--- heroku/run.sh
+++ heroku/run.sh
@@ -2,4 +2,6 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
+cp src/redis-server /usr/local/bin/
+cp src/redis-cli /usr/local/bin/
 redis-server
