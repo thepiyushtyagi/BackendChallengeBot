50ca24311576eb967e58821735d5fe1485997ac8
Piyush Tyagi <piyush.tyagi@go-mac-piyusht.local>
2019-11-06 01:40:50 +0530

    .

--- heroku/run.sh
+++ heroku/run.sh
@@ -1,11 +1,6 @@
-wget http://mirror.centos.org/centos/6/os/x86_64/Packages/yum-3.2.29-81.el6.centos.noarch.rpm &&
-rpm -ivh yum-3.2.29-81.el6.centos.noarch.rpm &&
-yum check-update &&
-yum update &&
-yum install -y sudo &&
-sudo apt-get update &&
-sudo apt-get install build-essential &&
-sudo apt-get install tcl8.5 &&
+apt-get update &&
+apt-get install build-essential &&
+apt-get install tcl8.5 &&
 wget http://download.redis.io/redis-stable.tar.gz &&
 tar xvzf redis-stable.tar.gz &&
 cd redis-stable &&
