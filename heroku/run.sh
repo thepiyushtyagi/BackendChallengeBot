4559ddea8a9a5e72e0e1ace912f230b4c3b331c9
Piyush Tyagi <piyush.tyagi@go-mac-piyusht.local>
2019-11-06 01:37:35 +0530

    .

--- heroku/run.sh
+++ heroku/run.sh
@@ -1,3 +1,7 @@
+wget http://mirror.centos.org/centos/6/os/x86_64/Packages/yum-3.2.29-81.el6.centos.noarch.rpm &&
+rpm -ivh *.rpm &&
+yum check-update &&
+yum update &&
 yum install -y sudo &&
 sudo apt-get update &&
 sudo apt-get install build-essential &&
