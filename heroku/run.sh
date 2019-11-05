fc8583cc69360ac9d20ee4c1c67b2b0d7089d709
Piyush Tyagi <piyush.tyagi@go-mac-piyusht.local>
2019-11-06 01:49:33 +0530

    .

--- heroku/run.sh
+++ heroku/run.sh
@@ -2,6 +2,5 @@ wget http://download.redis.io/redis-stable.tar.gz &&
 tar xvzf redis-stable.tar.gz &&
 cd redis-stable &&
 make &&
-make test &&
 src/redis-server --daemonize yes &&
 src/redis-cli PING
