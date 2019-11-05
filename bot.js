6d05f0b44db15a275056de590bbc259221b23791
Piyush Tyagi <piyush.tyagi@go-mac-piyusht.local>
2019-11-05 22:13:50 +0530

    google search feature added

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
+let request = require('request');
     // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -17,14 +18,20 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+    let searchKey = process.env.searchKey;
+    let searchEngineId = process.env.searchEngineId;
+    let googleSearchApi = `https://www.googleapis.com/customsearch/v1?key=${searchKey}&cx=${searchEngineId}&num=5&q=`;
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-
+        let searchString = null;
         args = args.splice(1);
+        if (args.length > 0){
+            searchString = args.join(' ')
+            googleSearchApi += searchString;
+        }
         switch(cmd) {
+
             // !ping
             case 'ping':
                 bot.sendMessage({
@@ -44,6 +51,36 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'hey!'
                 });
                 break;
+            case 'google':
+                request.get(googleSearchApi, function (err, resp, response) {
+                    if (err){
+                        return bot.sendMessage({
+                            to: channelID,
+                            message: `getting error ${err} in google search api`
+                        });
+                    }
+                    let topLinks = [];
+                    try{
+                        let topItems = JSON.parse(response).items;
+                        if (topItems){
+                            topItems.forEach((item) => {
+                                topLinks.push(item.link);
+                            });
+                        }
+                        topLinks.forEach((link) => {
+                            bot.sendMessage({
+                                to: channelID,
+                                message: link
+                            });
+                        })
+                    }catch (e) {
+                        return bot.sendMessage({
+                            to: channelID,
+                            message: `getting error ${e} in google search api`
+                        });
+                    }
+                })
+
             // Just add any case commands if you want to..
         }
     }
